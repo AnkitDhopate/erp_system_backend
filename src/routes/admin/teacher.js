45eb1c672b93b2d31ea9dfb4cbc5ae59833e8ee9
Swapnilnadekar <79741008+Swapnilnadekar@users.noreply.github.com>
2021-12-15 20:40:16 +0530

    fetch all student data in backend

--- src/routes/admin/teacher.js
+++ src/routes/admin/teacher.js
@@ -3,11 +3,13 @@ const {
   teacherRegister,
   teacherSignin,
   teacherSignout,
+  getAllStudentData,
 } = require("../../controllers/admin/teacher");
 const router = express.Router();
 
 router.post("/teacher/register", teacherRegister);
 router.post("/teacher/signin", teacherSignin);
 router.post("/teacher/signout", teacherSignout);
+router.get("/teacher/get-all-student-data", getAllStudentData)
 
 module.exports = router;
